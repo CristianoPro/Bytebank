42be9f54f5363941ddc4426abedfae6f914b05ac
Cristiano Oliveira <cristiano.so@hotmail.com>
2019-09-10 00:16:35 -0300

    Sobrecarga do metodo ToString nas Class ContaCorrente e ContaPoupanca

--- src/br/com/bytebank/banco/modelo/ContaCorrente.java
+++ src/br/com/bytebank/banco/modelo/ContaCorrente.java
@@ -23,4 +23,9 @@ public class ContaCorrente extends Conta implements Tributavel {
 		return super.saldo * 0.01;
 	}
 	
+	@Override
+	public String toString() {
+		return "ContaCorrente, Numero: " + super.getNumero();
+	}
+	
 }
