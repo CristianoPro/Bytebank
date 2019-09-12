ad140f74a53ae653ddc0316bbbbd9b01ab187f2a
Cristiano Oliveira <cristiano.so@hotmail.com>
2019-09-12 14:23:46 -0300

    sobreescrevendo metodo equals para verificar se já existe uma conta na lista

--- src/br/com/bytebank/banco/modelo/Conta.java
+++ src/br/com/bytebank/banco/modelo/Conta.java
@@ -95,6 +95,21 @@ package br.com.bytebank.banco.modelo;
     }
     
     @Override
+    public boolean equals(Object ref) {
+    	Conta outra = (Conta) ref;
+    	
+    	if(this.agencia != outra.agencia) {
+    		return false;
+    	}
+    	
+    	if(this.numero != outra.numero) {
+    		return false;
+    	}
+    	
+    	return true;    	
+    }
+    
+    @Override
 	public String toString() {
 		return "Numero: " + this.numero + ", Agencia: " + this.agencia;
 	}
--- src/br/com/bytebank/banco/test/util/TesteArrayList.java
+++ src/br/com/bytebank/banco/test/util/TesteArrayList.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 
-public class Teste {
+public class TesteArrayList {
 
 	public static void main(String[] args) {
 		ArrayList<Conta> lista = new ArrayList<Conta>();
